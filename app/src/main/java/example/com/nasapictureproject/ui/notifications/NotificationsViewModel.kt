370f629c926700e3a86bb984d32ad89670b87874
Ahmet Keskin <35667063+ahmetKeskiinn@users.noreply.github.com>
2021-07-31 12:54:10 +0300

    Project Initialize

---
+++ app/src/main/java/example/com/nasapictureproject/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package example.com.nasapictureproject.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
